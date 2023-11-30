640d4a18271edf7b232897d1355cc0d4a41d638c
Nasrullah <nasrullahzafar.dae@gmail.com>
2023-11-30 11:09:22 +0500

    Removed assistant_file_objecs

--- request.py
+++ request.py
@@ -36,27 +36,7 @@ form_data = {
 response_file = requests.post(url=url_file, headers=header_file, data=form_data, files=file)
 print(response_file.text)
 
-assistant_file_object = {
-    "object": "file",
-    "id": "file-CKtmztxbquCB5dTop4Y3EpV2",
-    "purpose": "assistants",
-    "filename": "about_nazia.txt",
-    "bytes": 2381,
-    "created_at": 1701244028, 
-    "status": "processed",
-    "status_details": None
-}
 
-assistant_file_object_2 = {
-  "object": "file",
-  "id": "file-XIlZkI1dOxYcguQEZzYq7fnX",
-  "purpose": "assistants",
-  "filename": "about_parveen_shakir.txt",
-  "bytes": 3546,
-  "created_at": 1701256113,
-  "status": "processed",
-  "status_details": None
-}
 
 # getting file list
 # file_list = requests.get(url="https://api.openai.com/v1/files", headers={"Authorization": f"Bearer {key}"})
